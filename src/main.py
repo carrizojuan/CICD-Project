04f40be5168de327ea45642e96df658e5cee9625
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2023-06-06 12:05:53 -0300

    Hello world in flask with tests

--- src/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    texto = "Hola Mundo"
-    #Prueba
-    print(texto)
-
-if __name__ == "__main__":
-    main()
---
+++ src/test_app.py
@@ -0,0 +1,21 @@
+import unittest
+import app
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+    
+    def test_hello_world_route(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+    
+    def test_invalid_route(self):
+        response = self.app.get("/invalid")
+        self.assertEqual(response.status_code, 404)
+
+
+if __name__== '__main__':
+    unittest.main()
