b92e76c3d3febd938d886f61c559e4f2a899774a
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2023-06-10 19:51:46 -0300

    prueba cicd

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TE AMO MI AMOR HERMOSA!'
+    return 'Por favor decime que ANDAA'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
