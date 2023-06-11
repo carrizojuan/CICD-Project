94040e0025b83a37a155849f1bd13d3bba841e46
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2023-06-11 17:02:14 -0300

    otra prueba

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Si anda lloro'
+    return 'Sos mi amor te amo tanto vamos a tomar mate a la COSTA'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
