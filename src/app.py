9f0a21c909ebfb64df7392474de9bbe992e6ff02
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2023-06-15 16:36:18 -0300

    adding smell code #AB169

--- src/app.py
+++ src/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Pagina de Prueba'
+    g = "Pagina de Prueba"
+    return g
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
